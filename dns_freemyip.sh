92eeaf3673bafba09b933a46f34a7979e9169eb8
Joel Samson <joel.samson@hotmail.com>
2026-07-31 19:52:04 -0400

    Add files via upload

--- dns_freemyip.sh
+++ dns_freemyip.sh
@@ -6,8 +6,7 @@ Docs: github.com/acmesh-official/acme.sh/wiki/dnsapi2#dns_freemyip
 Options:
  FREEMYIP_Token API Token
 Issues: github.com/acmesh-official/acme.sh/issues/6247
-Author: Recolic Keghart <root@recolic.net>, @Giova96
-Modified by: ExtremeFiretop for ASUSWRT-Merlin compatibility
+Author: Recolic Keghart <root@recolic.net>, @Giova96, ExtremeFiretop
 '
 
 FREEMYIP_DNS_API="https://freemyip.com/update?"
@@ -74,12 +73,14 @@ _get_root() {
 # There is random failure while calling freemyip API too fast. This function automatically retry until success.
 _freemyip_get_until_ok() {
   _fmi_url="$1"
-  for i in $(seq 1 8); do
+  i=1
+  while [ "$i" -le 8 ]; do
     _debug "HTTP GET freemyip.com API '$_fmi_url', retry $i/8..."
     _fmi_response="$(_get "$_fmi_url")"
     printf '%s\n' "$_fmi_response" >&2
     printf '%s\n' "$_fmi_response" | grep OK && return 0
     _sleep 1 # DO NOT send the request too fast
+    i=$((i + 1))
   done
   _err "Failed to request freemyip API: $_fmi_url . Server does not say 'OK'"
   return 1
@@ -91,13 +92,15 @@ _is_root_domain_published() {
   _webroot="$(_get_root "$_fmi_d")"
 
   _info "Verifying '""$_fmi_d""' freemyip webroot (""$_webroot"") is not published yet"
-  for i in $(seq 1 3); do
+  i=1
+  while [ "$i" -le 3 ]; do
     _debug "'$_webroot' ns lookup, retry $i/3..."
     if [ "$(_ns_lookup "$_fmi_d" TXT)" ]; then
       _debug "'$_webroot' already has a TXT record published!"
       return 0
     fi
     _sleep 10 # Give it some time to propagate the TXT record
+    i=$((i + 1))
   done
   return 1
 }
